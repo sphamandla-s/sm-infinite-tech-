ea0cc5770146bd490a2a2339bcb430efc1aa0db3
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2025-06-26 12:18:09 +0200

    fixing the workflow issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Key change for static export
+  trailingSlash: true, // Ensures correct file paths
+  images: {
+    unoptimized: true, // Disables Image Optimization API (not needed for static)
+  }
 };
 
 export default nextConfig;
