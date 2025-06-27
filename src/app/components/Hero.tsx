246a8d41233324b7bdd210425f2f56c85001b9f8
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2025-06-27 09:00:34 +0200

    updated the hero section colors

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -144,12 +144,16 @@ export function Hero() {
                     }}
                     className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
                 >
-                    <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-                        Explore Now
-                    </button>
-                    <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
-                        Contact Support
-                    </button>
+                    <Link href='/contact' className=" flex items-center justify-center gap-3 w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+                        <IoRocketOutline className="text-xl" />
+                        <span>Start a Project</span>
+                    </Link>
+                    <Link href="/SM-Infinite-Tech-Brochure.pdf"
+                        download className="flex items-center justify-center gap-3 w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
+                        <HiOutlineDownload className="text-xl" />
+
+                        Download Brochure
+                    </Link>
                 </motion.div>
                 <motion.div
                     initial={{
