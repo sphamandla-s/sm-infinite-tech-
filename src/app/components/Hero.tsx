e96d7cf28ebc2f4c40b0b8be5f754d0929ce6d7a
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2025-06-27 05:10:20 +0200

    some changes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
             <TextHoverEffect text="SM Infinite Tech" />
             <p className=" font-extrabold text-2xl text-center">Coming soon.</p>
             <p className="text-lg md:text-xl text-gray-400 mb-8 max-w-2xl mx-auto text-center">
-                We&apos;re currently working behind the scenes on something amazing. Stay tuned for our official launch!
+                We&apos;re currently working behind the scenes on something amazing. { " "} Stay tuned for our official launch!
             </p>
         </div>
     );
